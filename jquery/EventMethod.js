9464aed54f6ca5a0a080cfef5e5870c6456a9ce8
cyangbo <Administrator@PC-201506290051>
2015-09-04 21:58:40 +0800

    jquery常用的事件方法

---
+++ jquery/EventMethod.js
@@ -0,0 +1,31 @@
+
+/** 通过点击button对p元素修改 */
+$(document).ready(function(){
+	
+	
+	//点击事件
+	/*$("button").click(function(){
+		$(this.).hide();
+	});*/
+	
+	//双击事件
+	/*$("button").dbclick(function(){
+		$(this.).hide();
+	});*/
+	
+	//得到焦点事件
+	/*$("button").mouseenter(function(){
+		$(this.).hide();
+	});*/
+	
+	//鼠标离开
+	/*$("button").mouseleave(function(){
+		$(this.).hide();
+	});*/
+	
+	
+	
+	
+	
+	
+});
