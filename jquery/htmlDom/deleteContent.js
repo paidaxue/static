76aaf9ee64435e5ae43e03cf84ea757cbad7a580
cyangbo <Administrator@PC-201506290051>
2015-09-05 21:13:28 +0800

    jQuery HTML DOM删除元素

---
+++ jquery/htmlDom/deleteContent.js
@@ -0,0 +1,14 @@
+/**
+ * 常用删除方法:
+ * remove	把元素都删除了
+ * empty	只删除元素里面的内容
+ */
+$(document).ready(function(){
+
+	$("#btn1").click(function(){		
+		//$("#p1").remove();
+		$("#p1").empty();
+	});
+
+	
+});
