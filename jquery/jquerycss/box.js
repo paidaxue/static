7a2443e28b5ef4dc7000b29caa1037241f264c6f
cyangbo <Administrator@PC-201506290051>
2015-09-07 21:59:46 +0800

    jQuery盒子模型

---
+++ jquery/jquerycss/box.js
@@ -0,0 +1,14 @@
+
+
+
+
+$(document).ready(function(){
+	
+	
+	alert($("#div").height());
+	alert($("#div").innerHeight());
+	alert($("#div").outerHeight());
+	alert($("#div").outerHeight(true));
+	
+	
+});
